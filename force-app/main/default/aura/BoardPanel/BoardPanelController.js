26ad68e823095a97ee0707be39dbe0e1bbecf9c8
Dilipan M <dilipan12m@gmail.com>
2022-11-20 00:39:00 +0530

    added initital js functions

--- force-app/main/default/aura/BoardPanel/BoardPanelController.js
+++ force-app/main/default/aura/BoardPanel/BoardPanelController.js
@@ -1,5 +1,11 @@
 ({
-    myAction : function(component, event, helper) {
+    startGame: function (component, event, helper) {
+        let gameModeComboBox = component.find("gameMode");
+        let gameMode = gameModeComboBox.get("v.value");
+        component.set("v.selectedMode", gameMode);
+    },
 
+    reshuffleBoard: function (component, event, helper) {
+        console.log("Reshuffle Board");
     }
-})
+});
